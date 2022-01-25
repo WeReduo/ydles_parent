7465ccdd782174197fd90a696cb00a271ca979eb
wenernduo <63387480+WeReduo@users.noreply.github.com>
2022-01-25 10:51:58 +0800

    eureka注册中心
    eureka注册中心

---
+++ ydles_eureka/src/main/java/com/ydles/eureka/EurekaAppilcation.java
@@ -0,0 +1,20 @@
+package com.ydles.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Project Name: ydl_parent
+ * File Name: com.ydles.eureka.EurekaAppilcation
+ *
+ * @version 1.0
+ * @author:wenerduo
+ * @Date: 2022/01/05/上午 9:18
+ * Copyright (c) 2022,
+ */
+     @SpringBootApplication
+   public class EurekaAppilcation {
+    public static void main(String[] args) {
+                       SpringApplication.run(EurekaAppilcation.class);
+    }
+}
